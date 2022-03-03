4a7db9a40c928f2f44f821604363d58f8b42fcab
etiennecollin <99756528+etiennecollin@users.noreply.github.com>
2022-03-03 11:52:16 -0500

    Adding algorithms.

---
+++ Python/done/insertion_sort.py
@@ -0,0 +1,11 @@
+def insertion_sort(array):
+	for i in range(1, len(array)):
+		currentValue = array[i]
+		currentPosition = i
+
+		while currentPosition > 0 and array[currentPosition - 1] > currentValue:
+			array[currentPosition] = array[currentPosition -1]
+			currentPosition -= 1
+		
+		array[currentPosition] = currentValue
+	return array
--- Python/linear_search.py
+++ Python/linear_search.py
@@ -1,4 +1,8 @@
+
+# Iterative search
 def linear_search(input, target):
 	for i in range(len(input)):
 		if input[i] == target:
 			return i
+
+# Recursive search
