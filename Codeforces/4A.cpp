24010a13d7aab23c524b74dd30ee5d9ab73d69c1
etiennecollin <99756528+etiennecollin@users.noreply.github.com>
2022-03-14 10:54:14 -0400

    Adding codeforce folder and first problem.

---
+++ Codeforces/4A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int w;
+
+int main(){
+	std::cin >> w;
+	if (w>=1 && w<=100 && w % 2 == 0 && w != 2)
+	{
+		std::cout << "YES\n";
+	} else{
+		std::cout << "NO\n";
+	}	
+}
